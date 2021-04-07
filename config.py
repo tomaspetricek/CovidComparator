d64660e88e41ab528392b1ae27e84289d9132857
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2021-04-07 14:05:14 +0200

    Add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+LOCAL_VACCINATION_DATASET = "local_vaccination_dataset.csv"
+INTERNATIONAL_VACCINATION_DATASET = "inter_vaccination_dataset.csv"
+LOCAL_STATS_DATASET = "local_stats_dataset.csv"
+INTERNATIONAL_STATS_DATASET = "inter_stats_dataset.csv"
+
+LOGGING_API_URL = ""
+
